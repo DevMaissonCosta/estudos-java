d2b91c61aec61f46825ad6a8302a661061683cad
Maisson Costa <167487135+DevMaissonCosta@users.noreply.github.com>
2026-06-20 23:25:03 -0300

    praticando o codigo

--- 01-fundamentos-java/CondicionaisComposta2.java
+++ 01-fundamentos-java/CondicionaisComposta2.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class CondicionaisComposta2 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+
         System.out.println("digite o seu ano de nascimento!!");
         int nascimento = teclado.nextInt();
-        int i = 2015 - nascimento;
-        if (i>=18) {
-            System.out.println("Maior");
-        }else {
-            System.out.println("De menor");
-        }
+
+        System.out.println("Digite o ano que estamos!");
+        int ano = teclado.nextInt();
+
+        int idade = ano - nascimento;
+        if (idade<16) {
+            System.out.println("não pode votar " + idade + " anos");
+        }else if (idade >= 16 && idade < 18 || idade > 70){
+            System.out.println("A votacao é opcional  " + idade + " anos");
+        } else
+            System.out.println("É de total obrigação votar" + idade + " anos");
     }
 }
