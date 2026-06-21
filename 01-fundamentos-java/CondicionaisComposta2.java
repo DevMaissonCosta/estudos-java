5d7a2ddb2110ca548902bae19a402cc04e55d239
Maisson Costa <167487135+DevMaissonCosta@users.noreply.github.com>
2026-06-20 23:22:53 -0300

    praticando

--- 01-fundamentos-java/CondicionaisComposta2.java
+++ 01-fundamentos-java/CondicionaisComposta2.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CondicionaisComposta2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("digite o seu ano de nascimento!!");
+        int nascimento = teclado.nextInt();
+        int i = 2015 - nascimento;
+        if (i>=18) {
+            System.out.println("Maior");
+        }else {
+            System.out.println("De menor");
+        }
+    }
 }
--- 01-fundamentos-java/CondicionaisSimples1.java
+++ 01-fundamentos-java/CondicionaisSimples1.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CondicionaisSimples1 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        float n1 = teclado.nextFloat();
+        float n2 = teclado.nextFloat();
+        float m = (n1 + n2) / 2;
+        System.out.println("Sua Média é: " + m);
+        if (m>9){
+            System.out.println("Parabens!!");
+        }
+    }
 }
