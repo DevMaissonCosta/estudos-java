330dd88e30164d5a56444f836b67043a175df2d6
Maisson Costa <167487135+DevMaissonCosta@users.noreply.github.com>
2026-06-24 07:06:32 -0300

    praticando a estrutura de repeticao do/while

---
+++ 01-fundamentos-java/EstruturaDeRepeticaoDo.java
@@ -0,0 +1,11 @@
+public class EstruturaDeRepeticaoDo {
+    public static void main(String[] args) {
+        int cc = 0;
+        do // -> Do sig: faça
+             {
+            System.out.println("Cambalhota");
+            cc++;
+        } while // -> enquanto
+        (cc < 4);
+    }
+}
---
+++ 01-fundamentos-java/ExercicioDeRepeticaoDo2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ExercicioDeRepeticaoDo2 {
+    public static void main(String[] args) {
+        int n, s = 0;
+        String resp;
+        Scanner teclado = new Scanner(System.in);
+    }
+}
