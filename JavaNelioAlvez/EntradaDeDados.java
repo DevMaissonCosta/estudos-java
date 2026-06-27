385a4c7bd980a2e300c67795bd3006b4d8d3d95b
Maisson Costa <167487135+DevMaissonCosta@users.noreply.github.com>
2026-06-27 06:59:19 -0300

    estudando entrada e dados, revendo casting e resolvendo a quebra de linha

---
+++ JavaNelioAlvez/EntradaDeDados.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EntradaDeDados {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        x = sc.nextInt();
+        System.out.println("Voce digitou: " + x);
+        sc.close();
+    }
+}
--- JavaNelioAlvez/EstruturaSequencial.java
+++ JavaNelioAlvez/EstruturaSequencial.java
@@ -3,10 +3,11 @@ import java.util.Locale;
 
 public class EstruturaSequencial {
     public static void main(String[] args) {
-  double x = 10.345676;
-        System.out.println(x);
-        System.out.printf("%.4f%n", x);
         Locale.setDefault(Locale.US);
-        System.out.printf("%.2f%n", x);
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais %n", nome, idade, renda);
+
     }
 }
