1e791ac557ce2dd5b9908636114876c1de7f1ac2
Maisson Costa <167487135+DevMaissonCosta@users.noreply.github.com>
2026-07-01 22:37:24 -0300

    hoje iniciei meu ultimo trabalho da faculdade

---
+++ JavaNelioAlvez/ExercicioArray.java
@@ -0,0 +1,10 @@
+public class ExercicioArray {
+    public static void main(String[] args) {
+        String[] nome = {"joao", "Fernando", "Lucas"};
+        int i = 0;
+        while(i < nome.length){
+            System.out.println("Nome: " + nome[i]);
+            i++;
+        }
+    }
+}
