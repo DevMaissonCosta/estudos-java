8c34fd6776acdbe0d206ab0450d6c2a8cf39e143
Maisson Costa <167487135+DevMaissonCosta@users.noreply.github.com>
2026-07-06 22:32:34 -0300

    voltando a praticar logica de programação

---
+++ AlgotimoNelioCurso/saidaDeDados.java
@@ -0,0 +1,21 @@
+package AlgotimoNelioCurso;
+
+public class saidaDeDados {
+    public static void main(String[] args) {
+        int a;
+        double b;
+        String c;
+        boolean d;
+
+        a = 32;
+        b = 2.34567;
+        c = "Bom dia!";
+        d = true;
+
+        System.out.println(a);
+        System.out.printf("%.2f%n", b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
