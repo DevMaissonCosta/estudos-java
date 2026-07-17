53bcfe6984872c41826b0a8cc275a13726d29fee
Maisson Costa <167487135+DevMaissonCosta@users.noreply.github.com>
2026-07-17 07:14:28 -0300

    entrando em funçoes e pesquisando mais a fundo

---
+++ JavaNelioAlvez/InformacoesPessoais.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InformacoesPessoais {
+    public static void main(String[] args) {
+        System.out.println(dobrar(2));
+    }
+    static int dobrar (int numero){
+        int resultado = numero * 2;
+        return resultado;
+    }
+
+}
+
+
---
+++ JavaNelioAlvez/toLowerCase.java
@@ -0,0 +1,9 @@
+public class toLowerCase {
+    public static void main(String[] args) {
+        String original = "adnfejwsfkew JSIJENIWNEI iwekofdnkweo M  PIMMWEP ";
+
+        String s01 = original.toLowerCase();
+        System.out.println(original);
+        System.out.println(s01);
+    }
+}
