9b949dd0b406f111459e52285280838759c0a241
Maisson Costa <167487135+DevMaissonCosta@users.noreply.github.com>
2026-08-04 20:56:50 -0300

    praticando membros estaticos

---
+++ POO/entities/CurrencyConverter.java
@@ -0,0 +1,10 @@
+package POO.entities;
+
+public class CurrencyConverter {
+
+    public static double IOF = 0.06;
+
+    public static double dollartoReal(double amount, double dollarPrice){
+        return amount * dollarPrice * (1.0 + IOF);
+    }
+}
